5a1e7a45b017cccc63dff8782e2c64dee5f5ec1b
TodorovicIgor <40495825+TodorovicIgor@users.noreply.github.com>
2020-04-15 21:08:27 +0200

    field done

---
+++ src/Field.scala
@@ -0,0 +1,32 @@
+class Field(val x: Int, val y: Int, val v: String = "", val isOriginal: Boolean = false) extends Position {
+  require(v.equals("") || v.toInt<10 && v.toInt>0)
+  def getVal: Serializable = {
+    v match { 
+      case "" => ""
+      case _ => v.toInt
+    }
+  }
+  def writeVal(v: String): Field = {
+    if (isOriginal) {
+      this
+    } else {
+      new Field(x = this.x, y = this.y, v = v, isOriginal = this.isOriginal)
+    }
+  }
+}
+
+//object test {
+//  def main(args: Array[String]): Unit = {
+//    val nonMutableField: Field = new Field(0, 0, "3", isOriginal = true)
+//    assert(nonMutableField.getVal == 3)
+//    val nonMutatedField: Field = nonMutableField.writeVal("0")
+//    assert(nonMutatedField.getVal == 3)
+//    assert(nonMutableField == nonMutatedField)
+//    val mutableField: Field = new Field(0, 0, isOriginal = false)
+//    assert(mutableField.getVal == "")
+//    val mutatedField: Field = mutableField.writeVal("9")
+//    assert(mutatedField.getVal == 9)
+//
+//    print("passed")
+//  }
+//}
--- src/Pen.scala
+++ src/Pen.scala
@@ -24,3 +24,20 @@ class Pen(val x: Int, val y: Int) extends Position {
     }
   }
 }
+
+//object test {
+//  def main(args: Array[String]): Unit = {
+//    val p00: Pen = new Pen(0,0)
+//    assert(p00.goLeft().x == 0)
+//    assert(p00.goRight().x == 1)
+//    assert(p00.goUp().y == 1)
+//    assert(p00.goDown().y == 0)
+//    val p88: Pen = new Pen(8,8)
+//    assert(p88.goLeft().x == 7)
+//    assert(p88.goRight().x == 8)
+//    assert(p88.goUp().y == 8)
+//    assert(p88.goDown().y == 7)
+//
+//    print("passed")
+//  }
+//}
